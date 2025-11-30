8cd6a13dbb2b626532d06b53ccd2e62f2913f795
tunamoon <tunamoon@sas.upenn.edu>
2025-11-29 19:00:46 -0800

    changed header, add casualty page

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import  { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,7 +9,15 @@ import MembersScreen from './screens/MembersScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import LandingScreen from "./screens/LandingScreen.js";
 import CommanderTodoScreen from "./screens/CommanderTodoScreen";
+import AddPersonScreen from './screens/AddPersonScreen.js';
 
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
 
 const Tab = createBottomTabNavigator();
 const AuthStack = createNativeStackNavigator();
@@ -16,26 +25,92 @@ const CommanderTodoStack = createNativeStackNavigator();
 const HomeStack = createNativeStackNavigator();
 const ProfileStack = createNativeStackNavigator();
 const MembersStack = createNativeStackNavigator();
+const AddPersonStack = createNativeStackNavigator();
+
+// i need to override react native's header in order to increase size of app
+// change fonts, bg colors of header etc here
+const CustomHeader = () => (
+  <View
+    style={{
+      height: 120,              
+      backgroundColor: '#011F5B',
+      paddingTop: 52,
+      justifyContent: 'center',
+    }}
+  >
+    <AppHeaderTitle />
+  </View>
+);
+
+const AppHeaderTitle = () => (
+  <View style={{ alignItems: 'center' }}>
+    <Text
+      style={{
+        fontFamily: 'Inter_700Bold',   
+        fontSize: 20,
+        color: '#FFFFFF',
+      }}
+    >
+      eMERTgency
+    </Text>
+    <Text
+      style={{
+        fontFamily: 'Inter_400Regular',
+        fontSize: 20,
+        color: '#FFFFFFB2'
+      }}
+    >
+      Mass Casualty Management
+    </Text>
+  </View>
+);
 
 function HomeStackScreen() {
   return (
-      <HomeStack.Navigator>
-        <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Navigator 
+        screenOptions={{
+          header: () => <CustomHeader />,
+        }}
+        >
+        <HomeStack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          />
       </HomeStack.Navigator>
   );
 }
 
 function ProfileStackScreen() {
   return (
-      <ProfileStack.Navigator>
+      <ProfileStack.Navigator 
+        screenOptions={{
+          header: () => <CustomHeader />,
+        }}
+      >
         <ProfileStack.Screen name="Profile" component={ProfileScreen} />
       </ProfileStack.Navigator>
   );
 }
 
+function AddPersonStackScreen() {
+  return (
+    <AddPersonStack.Navigator 
+      screenOptions={{
+        header: () => <CustomHeader />,
+      }}
+    >
+      <AddPersonStack.Screen name="Add" component={AddPersonScreen} />
+    </AddPersonStack.Navigator>
+  )
+}
+
 function MembersStackScreen() {
   return (
-      <MembersStack.Navigator>
+      <MembersStack.Navigator 
+        screenOptions={{
+          header: () => <CustomHeader />,
+        }}
+      >
         <MembersStack.Screen name="Members" component={MembersScreen} />
         <MembersStack.Screen name="Settings" component={SettingsScreen} />
       </MembersStack.Navigator>
@@ -43,7 +118,11 @@ function MembersStackScreen() {
 }
 function CommanderTodoStackScreen() {
   return (
-      <CommanderTodoStack.Navigator>
+      <CommanderTodoStack.Navigator 
+        screenOptions={{
+          header: () => <CustomHeader />,
+        }}
+      >
         <CommanderTodoStack.Screen name="CommanderTodo" component={CommanderTodoScreen} />
       </CommanderTodoStack.Navigator>
   );
@@ -58,6 +137,7 @@ function MainTabNavigator() {
     >
       <Tab.Screen name="HomeNav" component={HomeStackScreen} />
       <Tab.Screen name="ProfileNav" component={ProfileStackScreen} />
+      <Tab.Screen name="AddNav" component={AddPersonStackScreen} />
       <Tab.Screen name="MembersNav" component={MembersStackScreen} />
       <Tab.Screen name="CommanderTodoNav" component={CommanderTodoStackScreen} />
     </Tab.Navigator>
